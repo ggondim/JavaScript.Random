26e4e31fad9068cccc3db9ef3b93518b5b142d62
Gustavo Gondim <gustavospgondim@gmail.com>
2016-04-06 13:56:57 -0300

    v0.0.1

--- random.js
+++ random.js
@@ -1,26 +1,28 @@
 function Random(precision) {
-  this.precision == precision || 0;
+    this.precision = precision || 0;
 }
 
-Random.prototype.next = function (max, opt_min) {
-  if ((max || null) === null) {
-    throw "Um valor máximo deve ser especificado.";
-  }
-  max = Number(max) === max && max % 1 !== 0 ? parseFloat(max) : parseInt(max);
-  var min = opt_min || 0;
-  var size = max.toString().length;
-  var random = parseFloat((Math.random() * Math.pow(10, size)).toFixed(this.precision));
-  if (random > max) {
-    if (size > 1) {
-      random = parseFloat((Math.random() * Math.pow(10, size - 1)).toFixed(this.precision));
-    } else {
-      while (random > max) {
-        random = parseFloat(Math.random().toFixed(this.precision));
-      }
+Random.prototype.next = function(max, opt_min) {
+    if ((max || null) === null) {
+        throw new Error("A maximum value must be specified.");
     }
-  }
-  if (random < min) {
-    random = min + random;
-  }
-  return random;
+    max = Number(max) === max && max % 1 !== 0 ? parseFloat(max) : parseInt(max);
+    var min = opt_min || 0;
+    min = Number(min) === min && min % 1 !== 0 ? parseFloat(min) : parseInt(min);
+
+    if (max <= min) {
+        return min;
+    };
+
+    var size = max.toString().split('.')[0].length;
+    if (max < 1) {
+        size = -1;
+    }
+    var random = parseFloat((Math.random() * Math.pow(10, size)).toFixed(this.precision));
+    while (random > max || random < min) {
+        random = parseFloat((Math.random() * Math.pow(10, size)).toFixed(this.precision));
+    }
+    return random;
 }
+
+exports.module = Random;
